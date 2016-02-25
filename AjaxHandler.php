7cbb71b43e2dde8f1013281dea11b17745d8e28b
vishal chowdhary <vcmyself@gmail.com>
2016-02-25 13:06:53 +0530

    Ajax Calling on Registering User // Back end Coding //

---
+++ AjaxHandler.php
@@ -0,0 +1,69 @@
+<?php 
+require('configuration/configuration.php');
+$oGeneral = new GeneralClass();
+ 
+$token= $_REQUEST['Token'];
+
+switch($token)
+{
+	case'save-govemp': 
+						$sEmail=$_REQUEST['fld_email'];
+						  
+						$oGeneral->check_duplicate_record('tbl_gov_emp','fld_email',$sEmail);
+						
+						$iCount=$oGeneral->icount;
+						if($iCount=="")
+						{	
+							$bData=$_POST;
+							$lastid = $oGeneral->insert_data('tbl_gov_emp',$bData);
+
+							if($lastid>0){
+							 
+							echo $msg ="Your position will be verified shortly. A confirmation will be sent to your e-mail.";	 	
+							}
+							else{
+							   echo $msg ="Please Try Again";
+							}
+						}
+						else{
+						  	
+						 echo $msg ="You are Already Registered";
+						}
+		
+	break;
+	
+	case'save-citizen': 
+	
+						$sEmail=$_REQUEST['fld_email'];
+						  
+						$oGeneral->check_duplicate_record('tbl_citizen','fld_email',$sEmail);
+						
+						$iCount=$oGeneral->icount;
+						if($iCount=="")
+						{	
+	
+	
+							$bData=$_POST;
+							$lastid = $oGeneral->insert_data('tbl_citizen',$bData);
+
+							if($lastid>0){
+
+							 echo $msg ="Citizen Added Successfully ";	 
+							}
+							else{
+							echo $msg ="Please Try Again";
+							}
+					    }
+						else{
+						  	
+						 echo $msg ="You are Already Registered";
+						}
+
+		
+	break;
+					
+							
+ 
+}
+
+?>
